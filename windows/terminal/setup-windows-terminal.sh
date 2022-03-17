383344bb46c7beea1fb2f589cfa12c8176f571a2
vanbrandaos <vanbrandaos@gmail.com>
2022-03-17 15:25:21 -0300

    enviando settings.json

--- windows/terminal/setup-windows-terminal.sh
+++ windows/terminal/setup-windows-terminal.sh
@@ -21,6 +21,18 @@ git clone https://aur.archlinux.org/nerd-fonts-fantasque-sans-mono.git
 cd nerd-fonts-fantasque-sans-mono
 makepkg -si
 
+cd /tmp
+wget https://github.com/belluzj/fantasque-sans/releases/download/v1.8.0/FantasqueSansMono-Normal.zip
+unzip FantasqueSansMono-Normal
+cd FantasqueSansMono-Normal/TTF
+sudo cp FantasqueSansMono-Bold.ttf /mnt/c/Windows/Fonts
+sudo cp FantasqueSansMono-BoldItalic.ttf /mnt/c/Windows/Fonts
+sudo cp FantasqueSansMono-Italic.ttf /mnt/c/Windows/Fonts
+sudo cp FantasqueSansMono-Regular.ttf /mnt/c/Windows/Fonts
+cd /tmp
+rm -rf FantasqueSansMono-Normal
+rm -rf FantasqueSansMono-Normal.zip
+
 cd /tmp 
 git clone https://github.com/agura-lex/find-the-command.git
 cd find-the-command
