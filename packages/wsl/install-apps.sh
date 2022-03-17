ed2ec7a19f14ce468762e5fa1fba595acba0b3b9
Vanessa Brandao <vanbrandaos@gmail.com>
2022-03-17 17:06:03 -0300

    install-apps.sh

--- packages/wsl/install-apps.sh
+++ packages/wsl/install-apps.sh
@@ -1,3 +1,16 @@
-#!bin/bash
+#!/bin/bash
 
-pacman -Syu --needed $(<pkg.list tr "\n" " ")
+pacman -Syu --needed $(< pkg.list tr "\n" " ")
+
+
+#groupadd docker
+
+#usermod -aG docker "$USER"
+
+#echo "127.0.0.1 desktop.cepel.br" >> /etc/hosts
+#echo "127.0.0.1 $HOSTNAME.cepel.br" >> /etc/hosts
+
+#add 
+
+# echo "listen_addresses = '*'" >> /var/lib/posgres/data/postgres.conf
+# echo "host all all 172.17. 0.0/16 trust" >> /var/lib/postgres/data/pg_hba.conf
