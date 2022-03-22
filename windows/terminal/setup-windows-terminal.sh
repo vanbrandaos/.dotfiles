275aa7a5aefe62537403ae28e9bfb714fb2cf404
Vanessa Brandao <vanbrandaos@gmail.com>
2022-03-22 14:53:30 -0300

    acertando fontes e setup para o terminal

--- windows/terminal/setup-windows-terminal.sh
+++ windows/terminal/setup-windows-terminal.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+echo '### ATENÇÃO: Ao final do script instale a fonte "FantasqueSansMono Nerd Font" na janela do explorer que abrirá.'
+echo '### Opcionalmente instale todas as fontes. Caminho Windows: C:wsl/fonts/fantasqueSansMono'
+echo '## Para a instalação do find-the-command, escolha a versão git'
+echo '---------'
+echo '---------'
+echo '---------'
 echo 'Insira o nome do seu usuário Windows'
 read WINDOWS_USER
 
@@ -16,19 +23,6 @@ fi
 sudo cp settings.json /mnt/c/Users/"$WINDOWS_USER"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState
 #ln -s ~/.dotfiles/windows/terminal/settings.json /mnt/c/Users/"$WINDOWS_USER"/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
 
-cd /tmp
-git clone https://aur.archlinux.org/nerd-fonts-fantasque-sans-mono.git
-cd nerd-fonts-fantasque-sans-mono
-makepkg -si
-
-cd /tmp
-wget https://github.com/belluzj/fantasque-sans/releases/download/v1.8.0/FantasqueSansMono-Normal.zip
-mkdir -p fonts/fantasqueSansMono-Normal
-sudo unzip FantasqueSansMono-Normal.zip -d /tmp/fonts/fantasqueSansMono-Normal
-cd fonts/fantasqueSansMono-Normal/TTF
-cd /tmp
-rm -rf FantasqueSansMono-Normal.zip
-
 cd /tmp 
 git clone https://github.com/agura-lex/find-the-command.git
 cd find-the-command
@@ -36,14 +30,21 @@ yay -S find-the-command
 #makepkg -si
 
 cd /tmp
-rm -rf nerd-fonts-fantasque-sans-mono
 rm -rf find-the-command
 
 chsh -s /usr/bin/fish
 
-echo '### ATENÇÃO: COPIE AS FONTES EM /tmp/fonts (sempre pasta TTF) para o windows, deixando em C:\Windows\Fonts'
+cd /tmp
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FantasqueSansMono.zip
+mkdir -p fonts/fantasqueSansMono
+sudo unzip FantasqueSansMono.zip -d /tmp/fonts/fantasqueSansMono
+cd /tmp
+rm -rf FantasqueSansMono.zip
+mkdir -p /mnt/c/wsl
+mv fonts /mnt/c/wsl
+cd /mnt/c/wsl/fonts/fantasqueSansMono
+powershell.exe /c start .
+cd ~/.dotfiles/windows/terminal
+
 echo '### Concluído'
-echo 'Reinicie o WSL'
-
-
-
+echo 'Instale as fontes e reinicie o WSL'
