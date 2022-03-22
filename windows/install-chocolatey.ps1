3150d484c23ab61da3dd9eee43c710f19be37514
Vanessa Brandao <vanbrandaos@gmail.com>
2022-03-22 11:20:37 -0300

    inserindo script chocolatey

--- windows/install-chocolatey.ps1
+++ windows/install-chocolatey.ps1
@@ -2,7 +2,7 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 #to run in powershell: powershell -executionpolicy bypass -File .\install-chocolatey.ps1
-
+#irst installation: Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 
 
