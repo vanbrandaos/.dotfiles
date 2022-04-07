87abdbab05286ef5f3cfd138a88512df5113edfd
Vanessa Brandao <vanbrandaos@gmail.com>
2022-04-07 09:07:48 -0300

    alterando caminhos

--- dotfiles-windows/wsl/install-arch.ps1
+++ dotfiles-windows/wsl/install-arch.ps1
@@ -7,13 +7,13 @@ if (!(Test-Path -Path "C:\wsl\Arch")) {
 }
 
 $default_url = "https://github.com/yuk7/ArchWSL/releases/download/22.3.18.0/Arch.zip"
-echo "Download from: $default_url"
-$url = Read-Host ''Paste a new URL or confirm URL pressing enter''
+echo "Download Arch ZIP from: $default_url"
+$url = Read-Host ''Paste a new ZIP URL or confirm URL pressing enter''
 if ("" -eq $url){
     $url = $default_url
 }
 
-echo "Extraindo Arch de $url..."    
+echo "Extracting Arch $url..."    
 Invoke-WebRequest -Uri $url -OutFile "C:\wsl\Arch\Arch.zip"        
 Expand-Archive "C:\wsl\Arch\Arch.zip" -DestinationPath "C:\wsl\Arch\"
 Remove-Item "C:\wsl\Arch\Arch.zip"
