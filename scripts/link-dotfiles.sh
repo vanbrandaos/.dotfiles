0a844123312be960773623bb4845c5160a02e3b6
Vanessa Brandao <vanbrandaos@gmail.com>
2023-07-04 19:25:16 -0300

    changing stow command

--- scripts/link-dotfiles.sh
+++ scripts/link-dotfiles.sh
@@ -4,7 +4,13 @@ scripts/make-dirs.sh
 echo "Stowing config files..."
 
 cd stow
-stow -vt ~ *
+#stow -vt ~ *
+stow --target="$HOME" --dir="$HOME/.dotfiles/stow/" --stow apps/
+stow --target="$HOME" --dir="$HOME/.dotfiles/stow/" --stow fish/
+stow --target="$HOME" --dir="$HOME/.dotfiles/stow/" --stow git/
+stow --target="$HOME" --dir="$HOME/.dotfiles/stow/" --stow kitty/
+stow --target="$HOME" --dir="$HOME/.dotfiles/stow/" --stow starship/
+stow --target="$HOME" --dir="$HOME/.dotfiles/stow/" --stow vscode/
 
 #cd "$CRT_DIR"
 #cd "${0%/*}"/..
