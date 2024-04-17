e09665e14e1e723a4edc5508ce93409b25d68993
Vanessa Brandao <vanbrandaos@gmail.com>
2024-04-17 14:31:33 -0300

    feat: tomcat envs

---
+++ tomcat/setenv.sh
@@ -0,0 +1,5 @@
+#JPDA_TRANSPORT="dt_socket"
+#JPDA_ADDRESS="8000"
+JPDA_SUSPEND="y"
+#JPDA_OPTS="-agentlib:jdwp=transport=$JPDA_TRANSPORT,address=$JPDA_ADDRESS,server=y,suspend=$JPDA_SUSPEND"
+#CATALINA_OPTS="$CATALINA_OPTS $JPDA_OPTS"
